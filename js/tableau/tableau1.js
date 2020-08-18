00ac7086041b21b359fce77354af717b171a71e4
abhisshekkumar <abhisshekkumar@umm.edu>
2020-08-18 18:20:09 -0400

    modified

--- js/tableau/tableau1.js
+++ js/tableau/tableau1.js
@@ -1,6 +1,6 @@
 async function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/WorldIndicators/GDPpercapita";
+    url = "https://public.tableau.com/views/Worldwide_Covid-19/ConfirmedDashboard";
     var options = {
       hideTabs: false,
       hideToolbar: false,
